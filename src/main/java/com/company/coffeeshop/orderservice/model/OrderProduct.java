eef07d954835be042338b77dee6a420ae6e1979c
tulio.henrique <tulio.henrique@axians.com>
2023-08-21 15:13:05 +0100

    Creating id mapping for OrderProduct

--- src/main/java/com/company/coffeeshop/orderservice/model/OrderProduct.java
+++ src/main/java/com/company/coffeeshop/orderservice/model/OrderProduct.java
@@ -3,6 +3,9 @@ package com.company.coffeeshop.orderservice.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -12,6 +15,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
 @EqualsAndHashCode(callSuper = true)
@@ -20,6 +25,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "order_products")
 public class OrderProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
